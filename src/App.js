148fc1c8e3d9e6d0e92aa64d850ef1baed099397
jmscottimd <jmscottimd@protonmail.com>
2019-08-15 20:39:36 -0400

    global css style with useContext

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 import SearchParams from "./SearchParams";
 
 const App = () => {
+  const themeHook = useState("darkblue");
   return (
-    <div>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      ;
-      <Router>
-        <SearchParams path="/" />
-        <Details path="/details/:id" />
-      </Router>
-    </div>
+    <React.StrictMode>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          ;
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
+    </React.StrictMode>
   );
 };
 
